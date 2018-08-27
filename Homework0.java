04c09370b321a726efc4ee6514e1dbd2b77bc677
Abby Forman <formaabb@berkeleyprep.org>
2018-08-27 08:01:14 -0400

    Abby Forman

--- Homework0.java
+++ Homework0.java
@@ -4,6 +4,10 @@
 
 public class Homework0 {
 
+	  public static void main(String[] args) {
+
+	    System.out.println("Hello, world!");
+
 	/*
 	 * Print out the line "Hello, World!"
 	 * Ensure that you match case and punctuation exactly
